f89cdabc58f5b6983d4a1ffb6000410765d752db
JOSE MARIA GARCIA DAROCA <110504961+JOSEDAROCA@users.noreply.github.com>
2022-08-04 12:03:16 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello FULL STACK BENITO JUAREZ!</h1>
+<div>
+  QUE CAPO JMGD ESTOY USANDO GITHUB Y VOY A HACR MI PRIMER COMMIT EN SANDBOX</a>.
+</div>`;
